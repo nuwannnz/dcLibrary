687a2a5883a67de4b082b17feddd891ea01e8149
Nuwan <nuwankarunarathna@hotmail.com>
2016-09-21 13:44:21 +0530

    home_template complete

---
+++ include/home_header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>dcLibrary</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="../css/main-styles.css" rel="stylesheet">
+        <link href="../css/home-template-styles.css" rel="stylesheet">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    </head>
+    <body>
+        <div class="root-container">
+
+            <div class="top-header site-accent-bg-dark">
+
+                <div class="content center-align-bl" style="min-height:80px;" >
+
+                    <div class="logo-container" style="margin-top:-24px;">
+                        <a href="#">
+                            <span class="site-font-l logo-large site-accent-fg">dc<span class="site-font-b">Library</span></span>
+                        </a>
+                    </div> <!-- end of logo container-->
+
+                    <div style="position:absolute;right:0px;top:50%;margin-top:-15px;" >
+                        <ul class="menu">
+                            <li><a class="border-button" href="#">Sign up</a></li>
+                            <li><a class="border-button" href="#">Sign in</a></li>
+                        </ul>
+                    </div>
+                </div> <!-- end of content container-->
+
+            </div> <!-- end of top header-->
---
+++ templates/home_template.php
@@ -0,0 +1,11 @@
+<?php require_once('../config.php') ?>
+
+<?php require_once($paths['include'] . '/home_header.php') ?>
+
+<!-- content -->
+<div class="content center-align-bl" style="min-height:770px;background-color:#fff;" >
+
+</div><!-- end of content container-->
+
+<?php require_once($paths['include'] . '/home_footer.php') ?>
+           
